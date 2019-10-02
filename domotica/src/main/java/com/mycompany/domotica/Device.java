e207416dd5d4fa349ec43a6345061349200a7602
alextorrer <alextorre0427@gmail.com>
2019-10-02 16:58:30 -0500

    Build Classes
    Build AdminDevices.java, Device.java, AC.java, Television.java, Light.java
    Add constructors on the device-derived classes

--- domotica/src/main/java/com/mycompany/domotica/Device.java
+++ domotica/src/main/java/com/mycompany/domotica/Device.java
@@ -10,6 +10,42 @@ public abstract class Device {
     private String model;
     private boolean status;
     
+    /*Constructor*/
+    public Device(String id, String name, String brand, String model){
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.model = model;
+    }
+    
+    /*Sets y Gets*/
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public String getModel(){
+        return model;
+    }
+    
+    
+    /*Métodos abstractos*/
     public abstract void turnOn();
     public abstract void turnOff();
 }
--- domotica/src/main/java/com/mycompany/domotica/Television.java
+++ domotica/src/main/java/com/mycompany/domotica/Television.java
@@ -3,6 +3,10 @@ package com.mycompany.domotica;
 
 public class Television extends Device{
     
+    public Television(String id, String name, String brand, String model){
+        super(id,name,brand,model);
+    }
+    
     @Override
     public void turnOn(){
         
