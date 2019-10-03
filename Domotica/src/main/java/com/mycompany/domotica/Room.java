27403e63d31d3acac9f0dc6547d7bf66cbc3cbf2
alextorrer <alextorre0427@gmail.com>
2019-10-02 22:02:28 -0500

    Update classes
    Fill up the classes

--- Domotica/src/main/java/com/mycompany/domotica/Room.java
+++ Domotica/src/main/java/com/mycompany/domotica/Room.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.domotica;
 
-/**
- *
- * @author alext
- */
+
 public class Room {
+    
+    private String id;
+    private String name;
+    private Device devices[];
     
 }
