8cbc0b72866789bb1adffbaebfdad501169c65f1
alextorrer <alextorre0427@gmail.com>
2019-10-03 01:33:40 -0500

    order

--- Domotica/src/main/java/com/mycompany/domotica/AC.java
+++ Domotica/src/main/java/com/mycompany/domotica/AC.java
@@ -6,11 +6,20 @@ public class AC extends Device implements Control_Temperature{
     
     private int temperature;
     
-    
+    /*Constructor que llama al de la superclase*/
     public AC(String id, String name, String brand, String model){
         super(id,name,brand,model);
     }
     
+    /*Set y Get*/
+    public void setTemperature(int temperature){
+        this.temperature = temperature;
+    }
+    public int getTemperature(){
+        return temperature;
+    }
+    
+    /*Funciones*/
     @Override
     public void turnOn(){
         
