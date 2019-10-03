c3f60894bf28952a7121c44de9899033ad3613e3
alextorrer <alextorre0427@gmail.com>
2019-10-03 00:35:02 -0500

    Build House.java

--- Domotica/src/main/java/com/mycompany/domotica/House.java
+++ Domotica/src/main/java/com/mycompany/domotica/House.java
@@ -3,7 +3,35 @@ package com.mycompany.domotica;
 
 public class House {
     
+    final int MAX_AREAS = 20;
     private String id;
     private String name;
     private Area areas[];
+    
+    /*Constructor*/
+    public House(String id, String name){
+        this.id = id;
+        this.name = name;
+        areas = new Area[MAX_AREAS];
+    }
+    
+    /*Set y Get*/
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAreas(Area areas[]){
+        this.areas = areas;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public Area[] getAreas(){
+        return areas;
+    }
 }
