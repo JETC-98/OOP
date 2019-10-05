02f017409872486a104b4a24013dd9b2562b1849
alextorrer <alextorre0427@gmail.com>
2019-10-05 12:09:25 -0500

    Create updateConfig()
    Method to update the house information

--- Domotica/src/main/java/com/mycompany/domotica/ConfigurateHouse.java
+++ Domotica/src/main/java/com/mycompany/domotica/ConfigurateHouse.java
@@ -2,18 +2,22 @@ package com.mycompany.domotica;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class ConfigurateHouse {
 
     House house;
     
+    /*Constructores*/
     public ConfigurateHouse(House house){
         this.house = house;
     }
+    public ConfigurateHouse(){}
     
-    /*Configuración inicial*/
-    public void configCreate(){
+    
+    /*Configuración inicial de la disposición de la casa*/
+    public void initConfig(){
 
        File file = new File("cfg.txt");
 
@@ -24,12 +28,36 @@ public class ConfigurateHouse {
                System.out.println("El archivo de la configuracion ya existe.");
            }  
 
-           FileWriter writer = new FileWriter(file);
-           writer.write((house.toStringHouse().toString()));
-           writer.close();
+           try (FileWriter writer = new FileWriter(file)) {
+               writer.write((house.toStringHouse().toString()));
+           }
 
        }catch (IOException e){
            e.printStackTrace();
        }
-    } 
+    }
+    
+    /*Actualizar la disposición de la casa*/
+    public void updateConfig(){
+        
+       String updatedData = house.toStringHouse().toString();
+       File file = new File("cfg.txt");
+       
+       try{
+           if(file.exists()){
+               System.out.println("Se ha realizado la actualización");
+           }else{
+              initConfig();
+               System.out.println("No existe la configuración inicial, creando...");
+           }
+           
+           FileWriter exFile = new FileWriter(file.getAbsoluteFile());
+           try (BufferedWriter upFile = new BufferedWriter(exFile)) {
+               upFile.write(updatedData);
+           }
+           
+       }catch (IOException e){
+           e.printStackTrace();
+       }
+    }
 }
